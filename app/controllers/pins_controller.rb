9f1fce0ffd37df08144c6c97a1286e6160483656
AJ James <ajjames66@yahoo.com>
2014-05-01 01:05:26 -0700

    Pin controller fixx

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,7 +2,7 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
   def index
-    @pins = Pin.all.order
+    @pins = Pin.all
   end
 
   def show
